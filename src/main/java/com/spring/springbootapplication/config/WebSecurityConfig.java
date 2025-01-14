b3b32b1f23f36439a06f32f237271a058be5be2e
Ryo-Umeda <ryo.umeda@prum.jp>
2025-01-14 16:52:00 +0900

    本番環境のBasic認証の無効化

--- src/main/java/com/spring/springbootapplication/config/WebSecurityConfig.java
+++ src/main/java/com/spring/springbootapplication/config/WebSecurityConfig.java
@@ -2,21 +2,22 @@ package com.spring.springbootapplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@Profile("dev")  // 開発環境専用の設定
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll()  // 全てのリクエストを許可
+                .requestMatchers("/**").permitAll()  // すべてのリクエストを許可
             )
-            .csrf(csrf -> csrf.ignoringRequestMatchers("/**"));  
+            .csrf(csrf -> csrf.disable())  // CSRFを無効化
+            .httpBasic(httpBasic -> httpBasic.disable())  // Basic認証を無効化
+            .formLogin(form -> form.disable());  // フォームログイン無効化（今後改変予定）
+
         return http.build();
     }
 }
